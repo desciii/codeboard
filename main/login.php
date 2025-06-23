36b1d972bb95020f92259eddb4b9624782ff374d
desciii <angelopanungcat@gmail.com>
2025-06-24 05:29:36 +0800

    fixed taskbar

--- main/login.php
+++ main/login.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login - Code Board</title>
+    <title>Login</title>
     <link rel="stylesheet" href="../css/login.css" />
     <link
       href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Roboto:wght@400;500&display=swap"
